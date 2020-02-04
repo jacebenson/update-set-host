14775f8b25bc0792d1c5bcb1e8532aa997a46a4c
Jace Benson <jace.benson@protonmail.com>
2020-02-04 10:15:27 -0600

    updated config

--- config.js
+++ config.js
@@ -1,11 +1,42 @@
+var port;
+var instance_id;
+var auth_enabled;
+var instance_properties;
+var password;
+if(process.env.PORT){
+    port = process.env.PORT
+} else {
+    port = '3000';
+}
+if(process.env.INSTANCE_ID){
+    instance_id = process.env.INSTANCE_ID
+} else {
+    instance_id = 'deadbeefdeadbeefdeadbeefdead8888';
+}
+if(process.env.AUTH_ENABLED){
+    auth_enabled = process.env.AUTH_ENABLED
+} else {
+    auth_enabled = 'true';
+}
+if(process.env.INSTANCE_PROPERTIES){
+    instance_properties = process.env.INSTANCE_PROPERTIES
+} else {
+    instance_properties = 'glide-orlando-12-11-2019__patch0-hotfix1-01-08-2020_01-12-2020_1944.zip';
+}
+if(process.env.PASSWORD){
+    password = process.env.PASSWORD
+} else {
+    password = 'test';
+}
+
 var config = {
-    port: '3000' || process.env.PORT,
-    instance_id: 'deadbeefdeadbeefdeadbeefdead8888' || process.env.INSTANCE_ID,
-    auth_enabled: true || process.env.AUTH_ENABLED,
-    instance_properties: 'glide-orlando-12-11-2019__patch0-hotfix1-01-08-2020_01-12-2020_1944.zip' || process.env.INSTANCE_PROPERTIES,
-    users: {}
+    port: port,
+    instance_id: instance_id,
+    auth_enabled: auth_enabled,
+    instance_properties: instance_properties,
+    users: {
+        'test': password
+    }
 };
-//var user = 'test' || process.env.USERNAME;
-config.users.test = process.env.PASSWORD;
 
 module.exports = config;
